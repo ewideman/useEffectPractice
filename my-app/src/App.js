683e9b6aacd59a59ac9d9990a78d878c8815ac38
ewideman <92744786+ewideman@users.noreply.github.com>
2022-01-27 14:13:27 -0500

    practice notes

---
+++ my-app/src/App.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import './App.css';
+
+function App() {
+  const [windowWidth, SetWindowWidth] = useState(window.innerWidth);
+  const [name, setName] = useState("");
+
+  //on every render 
+  useEffect(() => {
+    console.log("I re-rendered"); 
+  }); //No brakets for every use effect
+  
+// on first render/mount only! - componentDidMount Alternative
+  useEffect(() => {
+  console.log('the component Mounted')
+  }, []); 
+
+//on first render/whenever the dependacy changes 
+  useEffect(() => {
+    console.log('the name changed')
+  }, [name]);
+
+  //clean up code before running
+  useEffect(() => {
+    window.addEventListener("resize", updateWindowWidth);
+    return () => {
+      window.removeEventListener("resize", updateWindowWidth);
+    };
+
+  }, []);
+}
+
+export default App;
